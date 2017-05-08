bf2b5bf3b9797599d1a98a9b1b7424fe991e3173
DRAJ <davidrajnus@gmail.com>
2017-05-08 21:52:35 +0800

    changes to gemlock

--- Gemfile
+++ Gemfile
@@ -12,7 +12,7 @@ gem 'bootstrap-sass', '3.3.6'
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '~> 5.0.2'
 # Use sqlite3 as the database for Active Record
-gem 'sqlite3'
+gem 'pg'
 # Use Puma as the app server
 gem 'puma', '~> 3.0'
 # Use SCSS for stylesheets
@@ -37,10 +37,6 @@ gem 'jbuilder', '~> 2.5'
 
 # Use Capistrano for deployment
 # gem 'capistrano-rails', group: :development
-
-group :production do 
-  gem 'pg', '0.18.4'
-end
 
 group :test do
   gem 'rails-controller-testing', '0.1.1'
--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -5,17 +5,36 @@ module SessionsHelper
     session[:user_id] = user.id
   end
 
+  # Remembers a user in a persistent session.
+  def remember(user)
+    user.remember
+    cookies.permanent.signed[:user_id] = user.id
+    cookies.permanent[:remember_token] = user.remember_token
+  end
+
+  # def current_user
+  #   #this creates the variable current_user with the signed in user for other uses
+  #   @current_user ||= User.find_by(id: session[:user_id])
+  #   #the convention  ||= equates to @current_user equals to 
+  #   #@current_user or User.find_by. This way it only does the find_by
+  #   #if @current_user is already nil, not all the time someone logs in
+  # end
+
+  # Returns the user corresponding to the remember token cookie.
   def current_user
-    #this creates the variable current_user with the signed in user for other uses
     @current_user ||= User.find_by(id: session[:user_id])
-    #the convention  ||= equates to @current_user equals to 
-    #@current_user or User.find_by. This way it only does the find_by
-    #if @current_user is already nil, not all the time someone logs in
   end
 
   def logged_in?
-    @current_user.nil?
+    !current_user.nil?
   end
+
+  def forget(user)
+    user.forget
+    cookies.delete(:user_id)
+    cookies.delete(:remember_token)
+  end
+
 
   def log_out
     session.delete(:user_id)
